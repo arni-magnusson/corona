cd16e13f6da726b3d4af8f2fab57d196a0b269ef
Arni Magnusson <arni.magnusson@ices.dk>
2020-05-23 18:28:57 +0200

    Load lim() from icesTAF

--- install.R
+++ install.R
@@ -4,4 +4,5 @@ if(!any(installed == "remotes")) install.packages("remotes")
 
 if(!any(installed == "arni")) remotes::install_github("arni-magnusson/arni")
 if(!any(installed == "gplots"))  install.packages("gplots")
+if(!any(installed == "icesTAF"))  install.packages("icesTAF")
 if(!any(installed == "reshape2")) install.packages("reshape2")
