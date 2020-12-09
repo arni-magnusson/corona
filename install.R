1f7e664a0f8f9aeccbc7ff5bdda814f6a8ea38b3
Arni Magnusson <arni.magnusson@ices.dk>
2020-12-09 10:04:47 +0100

    Remove install.R

--- install.R
+++
@@ -1,8 +0,0 @@
-installed <- rownames(installed.packages())
-
-if(!any(installed == "remotes")) install.packages("remotes")
-
-if(!any(installed == "arni")) remotes::install_github("arni-magnusson/arni")
-if(!any(installed == "gplots"))  install.packages("gplots")
-if(!any(installed == "icesTAF"))  install.packages("icesTAF")
-if(!any(installed == "reshape2")) install.packages("reshape2")
