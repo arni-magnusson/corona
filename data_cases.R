1f387eed382b76498d53d4e7b5df2dbe575bd2a2
Arni Magnusson <thisisarni@gmail.com>
2022-02-05 09:45:42 +1100

    Prepare data by continent

--- data_cases.R
+++ data_cases.R
@@ -1,9 +1,10 @@
 ## Preprocess cases, write TAF data tables
 
-## Before: time_series_covid19_confirmed_global.csv,
+## Before: continents.csv, time_series_covid19_confirmed_global.csv,
 ##         UID_ISO_FIPS_LookUp_Table.csv (bootstrap/data)
-## After:  cases_current.csv, cases_doubling.csv, cases_rate.csv,
-##         cases_timeline.csv (data)
+## After:  cases_current.csv, cases_current_continent.csv,
+##         cases_doubling.csv, cases_rate.csv, cases_timeline.csv,
+##         cases_timeline_continent.csv (data)
 
 library(TAF)
 suppressPackageStartupMessages(library(gplots))  # rich.colors
@@ -15,28 +16,38 @@ mkdir("data")
 cases.global <-
   read.taf("bootstrap/data/time_series_covid19_confirmed_global.csv")
 lookup <- read.taf("bootstrap/data/UID_ISO_FIPS_LookUp_Table.csv")
+continents <- read.taf("bootstrap/data/continents.csv")
 
 ## Population
 pop <- lookup[lookup$Province_State=="", c("Country_Region","Population")]
 names(pop)[names(pop)=="Country_Region"] <- "Country"
 ## Omit Summer Olympics 2020, MS Zaandam, Diamond Princess
-countries <- pop$Country[!is.na(pop$Population)]
+countries <- sort(pop$Country[!is.na(pop$Population)])
+stopifnot(identical(countries, continents$Country))
 
 ## Reshape and calculate daily statistics
 timeline <- rearrange(cases.global, "Cases")
+timeline <- timeline[timeline$Country %in% countries,]  # actual countries
+timeline <- merge(timeline, continents)
+timeline <- timeline[c("Country", "Continent", "Date", "Cases")]
 timeline$Daily <- c(timeline$Cases[1], diff(timeline$Cases))
 timeline$Daily[timeline$Date == min(timeline$Date)] <-
   timeline$Cases[timeline$Date == min(timeline$Date)]
-timeline <- timeline[timeline$Country %in% countries,]  # actual countries
+timeline.c <- aggregate(cbind(Cases,Daily)~Date+Continent, timeline, sum)
+timeline.c <- timeline.c[c("Continent", "Date", "Cases", "Daily")]
+
 
 ## Current
 cases <- aggregate(Cases~Country, timeline, tail, 1)
 current <- merge(pop, cases)
+current <- merge(current, continents)
 current <- na.omit(current)
+current.c <- aggregate(cbind(Population,Cases)~Continent, current, sum)
 
 ## Rate (cases percent) and doubling time
 current$Rate <- round(current$Cases / current$Population * 100, 2)
 current$Doubling <- sapply(current$Country, doubling.time, column="Cases")
+current.c$Rate <- round(current.c$Cases / current.c$Population * 100, 2)
 
 rate <- current[current$Population>=1e5,]
 rate <- tail(rate[order(rate$Rate),], 25)
@@ -59,3 +70,5 @@ write.taf(current, "data/cases_current.csv", quote=TRUE)    # all countries
 write.taf(doubling, "data/cases_doubling.csv", quote=TRUE)  # lowest doubling
 write.taf(rate, "data/cases_rate.csv", quote=TRUE)          # highest rate
 write.taf(timeline, "data/cases_timeline.csv", quote=TRUE)  # timeline
+write.taf(current.c, "data/cases_current_continent.csv", quote=TRUE)
+write.taf(timeline.c, "data/cases_timeline_continent.csv", quote=TRUE)
--- data_deaths.R
+++ data_deaths.R
@@ -1,9 +1,10 @@
 ## Preprocess deaths, write TAF data tables
 
-## Before: time_series_covid19_deaths_global.csv,
+## Before: continents.csv, time_series_covid19_deaths_global.csv,
 ##         UID_ISO_FIPS_LookUp_Table.csv (bootstrap/data)
-## After:  deaths_current.csv, deaths_doubling.csv, deaths_rate.csv,
-##         deaths_timeline.csv (data)
+## After:  deaths_current.csv, deaths_current_continent.csv,
+##         deaths_doubling.csv, deaths_rate.csv, deaths_timeline.csv,
+##         deaths_timeline_continent.csv (data)
 
 library(TAF)
 suppressPackageStartupMessages(library(gplots))  # rich.colors
@@ -15,29 +16,38 @@ mkdir("data")
 deaths.global <-
   read.taf("bootstrap/data/time_series_covid19_deaths_global.csv")
 lookup <- read.taf("bootstrap/data/UID_ISO_FIPS_LookUp_Table.csv")
+continents <- read.taf("bootstrap/data/continents.csv")
 
 ## Population
 pop <- lookup[lookup$Province_State=="", c("Country_Region","Population")]
 names(pop)[names(pop)=="Country_Region"] <- "Country"
 ## Omit Summer Olympics 2020, MS Zaandam, Diamond Princess
-countries <- pop$Country[!is.na(pop$Population)]
+countries <- sort(pop$Country[!is.na(pop$Population)])
+stopifnot(identical(countries, continents$Country))
 
 ## Reshape and calculate daily statistics
 timeline <- rearrange(deaths.global, "Deaths")
+timeline <- timeline[timeline$Country %in% countries,]  # actual countries
 timeline$Deaths[timeline$Country=="Iceland" & timeline$Date=="2020-03-15"] <- 0
+timeline <- merge(timeline, continents)
+timeline <- timeline[c("Country", "Continent", "Date", "Deaths")]
 timeline$Daily <- c(timeline$Deaths[1], diff(timeline$Deaths))
 timeline$Daily[timeline$Date == min(timeline$Date)] <-
   timeline$Deaths[timeline$Date == min(timeline$Date)]
-timeline <- timeline[timeline$Country %in% countries,]  # actual countries
+timeline.c <- aggregate(cbind(Deaths,Daily)~Date+Continent, timeline, sum)
+timeline.c <- timeline.c[c("Continent", "Date", "Deaths", "Daily")]
 
 ## Current
 deaths <- aggregate(Deaths~Country, timeline, tail, 1)
 current <- merge(pop, deaths)
+current <- merge(current, continents)
 current <- na.omit(current)
+current.c <- aggregate(cbind(Population,Deaths)~Continent, current, sum)
 
 ## Rate (deaths per million) and doubling time
 current$Rate <- round(current$Deaths / current$Population * 1e6, 1)
 current$Doubling <- sapply(current$Country, doubling.time, column="Deaths")
+current.c$Rate <- round(current.c$Deaths / current.c$Population * 1e6, 1)
 
 rate <- current[current$Population>=1e5,]
 rate <- tail(rate[order(rate$Rate),], 25)
@@ -56,7 +66,9 @@ doubling$Color <- rev(rich.colors(max(doubling$Rank), "blues"))[doubling$Rank]
 doubling <- doubling[order(-doubling$Doubling,doubling$Rank),]
 
 ## Write tables
-write.taf(current, "data/deaths_current.csv", quote=TRUE)   # all countries
-write.taf(doubling, "data/deaths_doubling.csv", quote=TRUE) # lowest doubling
-write.taf(rate, "data/deaths_rate.csv", quote=TRUE)         # highest rate
-write.taf(timeline, "data/deaths_timeline.csv", quote=TRUE) # timeline
+write.taf(current, "data/deaths_current.csv", quote=TRUE)    # all countries
+write.taf(doubling, "data/deaths_doubling.csv", quote=TRUE)  # lowest doubling
+write.taf(rate, "data/deaths_rate.csv", quote=TRUE)          # highest rate
+write.taf(timeline, "data/deaths_timeline.csv", quote=TRUE)  # timeline
+write.taf(current.c, "data/deaths_current_continent.csv", quote=TRUE)
+write.taf(timeline.c, "data/deaths_timeline_continent.csv", quote=TRUE)
